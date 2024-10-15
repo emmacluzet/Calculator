f6b589ef8f8fad8c9230860c05147b452f27d20b
Emma Cluzet <cluzetemma@gmail.com>
2024-10-15 12:31:21 +0200

    implementation symbole scientifique

--- calculator.py
+++ calculator.py
@@ -7,6 +7,8 @@ fen.title("Calculator")
 # Buttons
 buttons=["7","8","9","/","4","5","6","*","1","2","3","+","0",".","C","-","="]
 
+science=["π","e","|x|","x²"]
+
 # Function called when a button is clicked
 def click(text_button):
     current = entry.get()
@@ -31,19 +33,36 @@ def click(text_button):
 
 # Create the display area (text entry for calculation)
 
-entry= tk.Entry(fen, width=18, font=("Arial", 18),borderwidth=2, relief="raised")
-entry.grid(row=0, column=0, columnspan=3)
+entry= tk.Entry(fen, width=30, font=("Arial", 18),borderwidth=2, relief="raised")
+entry.grid(row=0, column=0, columnspan=7)
 
-rowValue = 1
-colValue = 0
+rowValue = 3
+colValue = 4
 # Loop to create and place the buttons in a grid
 for button in buttons:
-    temp = lambda x=button: click(x)
-    tempo =  tk.Button(fen, text=button, width=5, height=2, font=("Arial", 18), command=temp,borderwidth=5,relief="ridge",activebackground="#c7ceff", background="#dbf0ff")
+    calcul = lambda x=button: click(x)
+    tempo =  tk.Button(fen, text=button, width=5, height=2, font=("Arial", 18), command=calcul,borderwidth=5,relief="ridge",activebackground="#c7ceff", background="#dbf0ff")
     tempo.grid(row=rowValue, column=colValue)
     colValue += 1
-    if colValue > 3:
-        colValue = 0
+    if colValue > 7:
+        colValue = 4
         rowValue += 1
 
+rowVal=3
+colVal=0
+for button in science:
+    calcul = lambda x=button: click(x)
+    tempo =  tk.Button(fen, text=button, width=5, height=2, font=("Arial", 18), command=calcul,borderwidth=5,relief="ridge",activebackground="#c7ceff", background="white")
+    tempo.grid(row=rowVal, column=colVal)
+    colVal += 1
+    if colVal > 3:
+        colVal = 0
+        rowVal += 1
+
+calcul2=0
+# RadioButton
+buttonN = tk.Radiobutton(fen, text="normal", value="n", command = calcul)
+buttonN.grid(row=2, column=0)
+buttonN = tk.Radiobutton(fen, text="scientifique", value="s", command = calcul)
+buttonN.grid(row=2, column=1)
 fen.mainloop()
